0b45eb04cd1560dd7f074314a87759ca146232b7
Rameez0510 <khanrameezrk428@gmail.com>
2026-07-24 20:20:53 +0530

    Implemented the function to initialize the max7219 device

--- max7219.h
+++ max7219.h
@@ -16,3 +16,38 @@
 #define Scan_Limit 0x0B
 #define Shutdown_Mode 0x0C
 #define Display_Test 0x0F
+
+//modes
+#define No_Decode 0x00
+#define Shutdown_Mode_ON 0x00
+#define Shutdown_Mode_OFF 0x01
+
+/**
+ * @brief Holds info about the max7219 device
+ */
+typedef struct{
+    spi_inst_t *spi_i; /**< SPI instance */
+    uint8_t CS_pin;
+    uint8_t displays;
+
+}max7219_t;
+
+
+/**
+ * @brief send command by choosing mode, value and display to use
+ * @brief choose display in bit format (eg. 0b1010)
+ * @param mx instance for max7219
+ * @param mode choose mode or digit register to use
+ * @param value value associated with the command
+ * @param display choose display(s) to be affected by this command, choose display in bit format (eg. 0b1010)
+ */
+void max7219_send_cmd(max7219_t mx, uint8_t mode, uint8_t value, uint8_t display);
+
+/**
+ * @brief initialize the max7219 device with no decode mode, minimum intensity, scan limit to all digits
+ * @param mx pointer to max7219_t instance
+ * @param spi_port instance for SPI communication
+ * @param cs_pin CS pin used
+ * @param displays Number of displays used
+ */
+void max7219_init(max7219_t *mx, spi_inst_t *spi_port, uint8_t cs_pin, uint8_t displays);
--- max7219_test.c
+++ max7219_test.c
@@ -12,60 +12,55 @@
 
 #define DISPLAYS 4 //add to max7219_t
 
-// int max7219_send_cmd(uint8_t mode, uint8_t value){
-//     // uint8_t data[] = {mode, value, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-//     // uint8_t data[] = {mode, value};
-//     // spi_write_blocking(SPI_PORT, data, 8);
-//     // spi_write_blocking(SPI_PORT, data, 2);
+void max7219_init(max7219_t *mx, spi_inst_t *spi_port, uint8_t cs_pin, uint8_t displays){
+    mx->spi_i = spi_port;
+    mx->CS_pin = cs_pin;
+    mx->displays = displays;
+
+    //reset registers
+    max7219_send_cmd(*mx, Decode_Mode, 0x00, 0b1111); //0b1111 --> 0xFF for initialize only
+    max7219_send_cmd(*mx, Intensity, 0x00, 0b1111);
+    max7219_send_cmd(*mx, Scan_Limit, 0x00, 0b1111);
+    max7219_send_cmd(*mx, Display_Test, 0x00, 0b1111); //turn off display test
+    max7219_send_cmd(*mx, Digit0, 0x00, 0b1111);
+    max7219_send_cmd(*mx, Digit1, 0x00, 0b1111);
+    max7219_send_cmd(*mx, Digit2, 0x00, 0b1111);
+    max7219_send_cmd(*mx, Digit3, 0x00, 0b1111);
+    max7219_send_cmd(*mx, Digit4, 0x00, 0b1111);
+    max7219_send_cmd(*mx, Digit5, 0x00, 0b1111);
+    max7219_send_cmd(*mx, Digit6, 0x00, 0b1111);
+    max7219_send_cmd(*mx, Digit7, 0x00, 0b1111);
+    max7219_send_cmd(*mx, Shutdown_Mode, 0x00, 0b1111); //turn on shutdown mode
+
+    //initialize with no decode mode, minimum intensity, scan limit to all digits
+    max7219_send_cmd(*mx, Decode_Mode, No_Decode, 0b1111);
+    max7219_send_cmd(*mx, Intensity, 0x00, 0b1111);
+    max7219_send_cmd(*mx, Scan_Limit, 0x07, 0b1111);
+
+    //turn off shutdown mode
+    max7219_send_cmd(*mx, Shutdown_Mode, 0x01, 0b1111); 
     
-//     // uint16_t data[] = {0, (mode<<8) | value, 0, 0};
-//     uint16_t data[] = {(mode<<8) | value, (mode<<8) | value, (mode<<8) | value, (mode<<8) | value};
-//     gpio_put(SPI_CS, 0);
-//     spi_write16_blocking(SPI_PORT, data, 4);
-//     gpio_put(SPI_CS, 1);
-//     printf("Mode: 0x%02X  Value: 0x%02X Data sent: 0x%04X\n", mode, value, data[0]);
-//     if (mode == Digit3 || mode == Digit4){
-//         sleep_ms(500);
-//     }
-// }
-int max7219_send_cmd(uint8_t mode, uint8_t value, uint8_t display){ //display 0b1010 type
+}
+
+void max7219_send_cmd(max7219_t mx, uint8_t mode, uint8_t value, uint8_t display){ //display 0b1010 type
 
     uint16_t data = (mode<<8) | value;
     uint16_t ignore = No_Op;
 
-    gpio_put(SPI_CS, 0);
-    for (int i = DISPLAYS-1; i >= 0; i--)
+    gpio_put(mx.CS_pin, 0);
+    for (int i = mx.displays-1; i >= 0; i--)
     {
         if (display & (1<<i))  
         {
-            spi_write16_blocking(SPI_PORT, &data, 1);
+            spi_write16_blocking(mx.spi_i, &data, 1);
         }else{
-            spi_write16_blocking(SPI_PORT, &ignore, 1);
+            spi_write16_blocking(mx.spi_i, &ignore, 1);
         }
     }
-    gpio_put(SPI_CS, 1);
-        
-    
-    // spi_write16_blocking(SPI_PORT, data, 4);
+    gpio_put(mx.CS_pin, 1);
     // printf("Mode: 0x%02X  Value: 0x%02X Data sent: 0x%04X\n", mode, value, data[0]);
 }
 
-int max7219_apply_to_one(uint8_t mode, uint8_t value, uint8_t display){
-    uint16_t data[] = {0, 0, 0, 0};
-    data[3-display] = (mode << 8) | value;
-    gpio_put(SPI_CS, 0);
-    spi_write16_blocking(SPI_PORT, data, 4);
-    gpio_put(SPI_CS, 1);
-    sleep_ms(250);
-    printf("Mode: 0x%02X  Value: 0x%02X Data sent: 0x%04X\n", mode, value, data[3-display]);
-}
-
-int max7219_send_single_cmd(uint8_t mode, uint8_t value){
-    uint16_t data = (mode << 8) | value;
-    gpio_put(SPI_CS, 0);
-    spi_write16_blocking(SPI_PORT, &data, 1);
-    gpio_put(SPI_CS, 1);
-}
 
 int main()
 {
@@ -82,60 +77,17 @@ int main()
 
     spi_set_format(SPI_PORT, 16, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
 
+    max7219_t mx;
     //initialize max7219
+    max7219_init(&mx, SPI_PORT, SPI_CS, 4);
+
     printf("Starting\n");
-    max7219_send_cmd(Decode_Mode, 0x00, 0b1111);
-    max7219_send_cmd(Intensity, 0x00, 0b1111);
-    max7219_send_cmd(Scan_Limit, 0x00, 0b1111);
-    max7219_send_cmd(Display_Test, 0x00, 0b1111); //turn off display test
-    // sleep_ms(2000);
-    max7219_send_cmd(Digit0, 0x00, 0b1111);
-    max7219_send_cmd(Digit1, 0x00, 0b1111);
-    max7219_send_cmd(Digit2, 0x00, 0b1111);
-    max7219_send_cmd(Digit3, 0x00, 0b1111);
-    max7219_send_cmd(Digit4, 0x00, 0b1111);
-    max7219_send_cmd(Digit5, 0x00, 0b1111);
-    max7219_send_cmd(Digit6, 0x00, 0b1111);
-    max7219_send_cmd(Digit7, 0x00, 0b1111);
-    max7219_send_cmd(Shutdown_Mode, 0x00, 0b1111); //turn on shutdown mode
 
-    max7219_send_cmd(Decode_Mode, No_Decode, 0b1111);
-    max7219_send_cmd(Intensity, 0x00, 0b1111);
-    max7219_send_cmd(Scan_Limit, 0x07, 0b1111);
-
-    max7219_send_cmd(Digit3, 0x3C, 0b1001);
-    max7219_send_cmd(Digit4, 0xC3, 0b0110);
-
-    max7219_send_cmd(Shutdown_Mode, 0x01, 0b1111); //turn off shutdown mode
-
-    sleep_ms(1000);
-
-    // max7219_send_cmd(Shutdown_Mode, 0x00);
-    // max7219_apply_to_one(Digit0, 0xF0, 3);
-    sleep_ms(1000);
-    // max7219_apply_to_one(Digit0, 0xF0, 1);
-    // max7219_send_cmd(Shutdown_Mode, 0x01);
-    // printf("0x%02X\n", 0b1010);
-    sleep_ms(1000);
-    // max7219_send_single_cmd(Digit2, 0xFF);
-    // sleep_ms(1000);
-    // max7219_send_single_cmd(Digit2, 0xFF);
-    // sleep_ms(1000);
-    // max7219_send_single_cmd(Digit2, 0xFF);
-    // sleep_ms(1000);
-    // max7219_apply_to_one(Digit0, 0b00000001, 1);
-
-
-
+    max7219_send_cmd(mx, Digit0, 0b11110000, 0b1000);
+    max7219_send_cmd(mx, Digit0, 0b10101010, 0b0001);
 
 
     while (true) {
         tight_loop_contents();
-        // sleep_ms(10);
-        // max7219_send_cmd(Display_Test, 0x00);
-        // max7219_send_cmd(Shutdown_Mode, 0x00);
-        // sleep_ms(1000);
-        // max7219_send_cmd(Shutdown_Mode, 0x01);
-        // sleep_ms(1000);
     }
 }
